2505c879298017615790b7cfe68093abd57d9425
Stanley Lageweg <stanleylageweg@gmail.com>
2026-05-23 15:58:11 +0200

    Correctly generate the minified javascript in the _site directory

--- _plugins/build_js.rb
+++ _plugins/build_js.rb
@@ -1,4 +1,4 @@
-JS__FILES = Dir[
+JS_FILES = Dir[
   "assets/js/vendor/jquery/jquery-3.6.0.js",
   "assets/js/plugins/*.js",
   "assets/js/custom/*.js",
@@ -7,30 +7,43 @@ JS__FILES = Dir[
 
 # Exclude the javascript files that we'll be minifying
 Jekyll::Hooks.register :site, :post_read do |site|
-  JS__FILES.each do |file_path|
+  JS_FILES.each do |file_path|
     site.static_files.delete_if { |file| file.relative_path == "/#{file_path}" }
   end
 end
 
 # Minify the javascript files
-Jekyll::Hooks.register :site, :post_write do |site|
-  target = File.join(site.dest, "assets/js/main.min.js")
+Jekyll::Hooks.register :site, :pre_render do |site|
+  target = "assets/js/main.min.js"
+  target_map = "assets/js/main.min.js.map"
 
-  target_mtime = File.exist?(target) ? File.mtime(target) : Time.at(0)
-  needs_rebuild = JS__FILES.any? { |file| File.mtime(file) > target_mtime }
-  next unless needs_rebuild
+  target_file = File.join(site.dest, target)
+  target_map_file = File.join(site.dest, target_map)
+  target_mtime = File.exist?(target_file) ? File.mtime(target_file) : Time.at(0)
+  target_map_mtime = File.exist?(target_map_file) ? File.mtime(target_map_file) : Time.at(0)
+  needs_rebuild = JS_FILES.any? { |file| File.mtime(file) > [target_mtime, target_map_mtime].min }
 
-  started_at = Time.now
-  Jekyll.logger.info("Javascript Minify:", "starting.")
+  if needs_rebuild
+    started_at = Time.now
+    Jekyll.logger.info("Javascript Minify:", "starting.")
 
-  system(
-    "npx", "uglifyjs",
-    "-c",
-    "--source-map",
-    "-m",
-    "-o", target,
-    *JS__FILES
-  ) or raise "JavaScript build failed"
+    system(
+      "npx", "uglifyjs",
+      "-c",
+      "--source-map",
+      "-m",
+      "-o", target_file,
+      *JS_FILES
+    ) or raise "JavaScript build failed"
 
-  Jekyll.logger.info("Javascript Minify:", "done in #{(Time.now - started_at).round(2)} seconds.")
+    Jekyll.logger.info("Javascript Minify:", "done in #{(Time.now - started_at).round(2)} seconds.")
+  else
+    Jekyll.logger.info("Javascript Minify:", "skipped, no changes detected.")
+  end
+
+  [target, target_map].each do |file|
+    unless site.keep_files.include?(file)
+      site.keep_files << file
+    end
+  end
 end
